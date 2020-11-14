ff811592d6e679678bdb0c7e1bf30d62a6d82d13
zemiret <dev@amleczko.pl>
2020-11-14 18:39:42 +0100

    gettin somewhere, trying different approach

--- zad2/robot.py
+++ zad2/robot.py
@@ -20,7 +20,6 @@ def is_read_valid(dist):
 # Maximum speed for the velocity value of the wheels.
 # Don't change this value.
 MAX_SPEED = 5.24
-MIN_SPEED = 3.0
 
 # Get pointer to the robot.
 robot = Robot()
@@ -85,20 +84,39 @@ while robot.step(timestep) != -1:
 # Main loop.
 integral = 0.0
 prev_skew = 0
-prev_skew_dist_so0 = 0
-prev_skew_dist_so15 = 0
-tau_p = 30.0
-tau_d = 15.0
-tau_i = 0.0
+# prev_skew_dist_so0 = 0
+# prev_skew_dist_so15 = 0
+# tau_p = 30.0
+# tau_d = 15.0
+# tau_i = 0.0
+
+parallel_skew = 0.0
+so0_skew = 0.0
+so1_skew = 0.0
+so2_skew = 0.0
+so3_skew = 0.0
+so4_skew = 0.0
+FRONT_VALID_READ = 0.7
+WALL_DIST = 0.5
 
 left_vel = MAX_SPEED
 right_vel = MAX_SPEED
 
 invalidated = False
 
+def pd_steer(left_vel, right_vel, cur_skew, prev_skew, tau_p, tau_d):
+    diff = (cur_skew - prev_skew) / timestep 
+    steer = tau_p * cur_skew + tau_d * diff
+
+    left_vel = max(min(MAX_SPEED, left_vel + steer), -MAX_SPEED)
+    right_vel = max(min(MAX_SPEED, right_vel - steer), -MAX_SPEED)
+
+    return left_vel, right_vel
+
+
 while robot.step(timestep) != -1:
-    so0_dist = getDistance(so0)
     so15_dist = getDistance(so15)
+    so0_dist = getDistance(so0)
 
     so1_dist = getDistance(so1)
     so2_dist = getDistance(so2)
@@ -110,179 +128,86 @@ while robot.step(timestep) != -1:
     so13_dist = getDistance(so13)
     so14_dist = getDistance(so14)
 
-    if so3_dist < 0.51 or so4_dist < 0.51:
-        # practically front to the wall
-        ## print('so3 valid')
-        print('Front to the wall: ', so3_dist, so4_dist)
-        invalidated = True
+    if invalidated:
+        left_vel = MAX_SPEED
+        right_vel = MAX_SPEED
+        invalidated = False
 
-        leftWheel.setVelocity(MAX_SPEED)
-        rightWheel.setVelocity(-MAX_SPEED)
-
-        while so3_dist < 2 or so4_dist < 2:
-            so3_dist = getDistance(so3)
-            so4_dist = getDistance(so4)
-            print('stepping here: ', so3_dist, so4_dist)
-            robot.step(timestep)
-
-
-#     if not is_read_valid(so0_dist) and is_read_valid(so15_dist): # left turn (I hope at least)
-#         # print('LEFT TURN!')
-#         left_vel = (MAX_SPEED * 3/4)
-#         right_vel = (MAX_SPEED)
-#         leftWheel.setVelocity(left_vel)
-#         rightWheel.setVelocity(right_vel)
-#         while not is_read_valid(so0_dist):
-#             robot.step(timestep)
-# 
-#         # print('BACK AT IT AGAIN')
-
-#    if is_read_valid(so0_dist) and is_read_valid(so15_dist):
-    # print(so0_dist, so15_dist)
-
-    if 0.45 < so0_dist < 0.55 and 0.45 < so15_dist < 0.55:
-        # keep current path
-        if invalidated:
-            left_vel = MAX_SPEED
-            right_vel = MAX_SPEED
-            invalidated = False
-
-        cur_skew = so15_dist - so0_dist # > 0 => going towards wall
-        diff = (cur_skew - prev_skew) / timestep # > the bigger abs value, the steeper the error func
-
-        steer = tau_p * cur_skew + tau_d * diff
-        prev_skew = cur_skew            
-
-        left_vel = max(min(MAX_SPEED, left_vel + steer), MIN_SPEED)
-        right_vel = max(min(MAX_SPEED, right_vel - steer), MIN_SPEED)
-#    elif not is_read_valid(so0_dist):
-    else:
-        # TODO: Idea. if both are valid, choose the bigger value
-        if is_read_valid(so0_dist):
-            if invalidated:
-                left_vel = MAX_SPEED
-                right_vel = MAX_SPEED
-                invalidated = False
-
-            cur_skew = 0.5 - so0_dist # > 0 = need to turn away from the wall
-            diff = (cur_skew - prev_skew_dist_so0) / timestep # > the bigger abs value, the steeper the error func
-
-            steer = tau_p * cur_skew + tau_d * diff
-            prev_skew_dist_so0 = cur_skew            
-            
-            # print('STEER: ', steer)
-
-            left_vel = max(min(MAX_SPEED, left_vel + steer), MIN_SPEED)
-            right_vel = max(min(MAX_SPEED, right_vel - steer), MIN_SPEED)
-
-        elif is_read_valid(so15_dist):
-            if invalidated:
-                left_vel = MAX_SPEED
-                right_vel = MAX_SPEED
-                invalidated = False
-
-            cur_skew = 0.5 - so0_dist # > 0 = need to turn away from the wall
-            diff = (cur_skew - prev_skew_dist_so15) / timestep # > the bigger abs value, the steeper the error func
-
-            steer = tau_p * cur_skew + tau_d * diff
-            prev_skew_dist_so15 = cur_skew            
-
-            left_vel = max(min(MAX_SPEED, left_vel + steer), MIN_SPEED)
-            right_vel = max(min(MAX_SPEED, right_vel - steer), MIN_SPEED)
-
-        ## print('So0 invalid')
-
-#        elif is_read_valid(so3_dist):
-#            # print('so3 valid')
-#            left_vel = MAX_SPEED
-#            right_vel = MAX_SPEED * 0.01
-#        elif is_read_valid(so2_dist):
-#            # print('so2 valid')
-#            left_vel = MAX_SPEED
-#            right_vel = MAX_SPEED * 0.01
-        elif so1_dist < 1.0:
-            invalidated = True
-            # # print('so1 valid')
-            left_vel = MAX_SPEED
-            # right_vel = MAX_SPEED * 0.01
-            right_vel = -MAX_SPEED
-
-            while not is_read_valid(so0_dist):
-                so0_dist = getDistance(so0)
-                robot.step(timestep)
-
-
-#         elif is_read_valid(so15_dist):
-#             # print('so15 valid')
-#             left_vel = MAX_SPEED * 0.01 
-#             right_vel = MAX_SPEED
-# 
-#         elif is_read_valid(so12_dist):
-#             # print('so12 valid')
-#             left_vel = 0.01
-#             right_vel = MAX_SPEED
-#         elif is_read_valid(so13_dist):
-#             # print('so13 valid')
-#             left_vel = MAX_SPEED * 0.01 
-#             right_vel = MAX_SPEED
-#         elif is_read_valid(so14_dist):
-#             # print('so14 valid')
-#             left_vel = MAX_SPEED * 0.01 
-#             right_vel = MAX_SPEED
-        else:
-            # nothing valid. Probably turn left
-            # print('so0 invalid, nothing valid')
-            invalidated = True
-            left_vel = MAX_SPEED * 0.4
-            right_vel = MAX_SPEED
-
-#    elif not is_read_valid(so15_dist):
+#    if so3_dist < 0.51 or so4_dist < 0.51:
+#        # practically front to the wall
+#        ## print('so3 valid')
+#        print('Front to the wall: ', so3_dist, so4_dist)
 #        invalidated = True
-#        # print('SO15 invalid')
-#        so12_dist = getDistance(so12)
-#        so13_dist = getDistance(so13)
-#        so14_dist = getDistance(so14)
 #
-#        if is_read_valid(so12_dist):
-#            # print('so12 valid')
-#            left_vel = MAX_SPEED * 0.01
-#            right_vel = MAX_SPEED
-#        elif is_read_valid(so13_dist):
-#            # print('so13 valid')
-#            left_vel = MAX_SPEED * 0.01
-#            right_vel = MAX_SPEED
-#        elif is_read_valid(so14_dist):
-#            # print('so14 valid')
-#            left_vel = MAX_SPEED * 0.01
-#            right_vel = MAX_SPEED
+#        leftWheel.setVelocity(MAX_SPEED)
+#        rightWheel.setVelocity(-MAX_SPEED)
+#
+#        while so3_dist < 2 or so4_dist < 2:
+#            so3_dist = getDistance(so3)
+#            so4_dist = getDistance(so4)
+#            print('stepping here: ', so3_dist, so4_dist)
+#            robot.step(timestep)
+
+    cur_par_skew = so15_dist - so0_dist
+    if is_read_valid(so0_dist) and is_read_valid(so15_dist)\
+            and (0.4 < so0_dist < 0.8 or 0.4 < so15_dist < 0.8)\
+            and -0.03 < cur_par_skew < 0.03:
+        # keep current path
+#        cur_skew = so15_dist - so0_dist # > 0 => going towards wall
+        left_vel, right_vel = pd_steer(left_vel, right_vel, cur_par_skew, parallel_skew, 0.3, 6.0)
+        parallel_skew = cur_par_skew
+
+        so0_skew = 0
+        so3_skew = 0
+        so2_skew = 0
+    else:
+#        if so4_dist < FRONT_VALID_READ:
+#            print('so4', so4_dist)
+#            cur_skew = WALL_DIST - so4_dist
+#            left_vel, right_vel = pd_steer(left_vel, right_vel, cur_skew, so4_skew, 10.0, 5.0)
+#            so4_skew = cur_skew
+        if so3_dist < FRONT_VALID_READ * 1.2:
+            print('so3', so3_dist)
+            cur_skew = WALL_DIST - so3_dist
+            left_vel, right_vel = pd_steer(left_vel, right_vel, cur_skew, so3_skew, 5.0, 5.0)
+            so3_skew = cur_skew
+
+            parallel_skew = 0
+            so0_skew = 0
+        if so2_dist < FRONT_VALID_READ * 1.2:
+            print('so2', so2_dist)
+            cur_skew = WALL_DIST - so2_dist
+            left_vel, right_vel = pd_steer(left_vel, right_vel, cur_skew, so2_skew, 3.0, 3.0)
+            so2_skew = cur_skew
+
+            parallel_skew = 0
+            so3_skew = 0
+            so0_skew = 0
+        elif so1_dist < FRONT_VALID_READ:
+            print('so1', so1_dist)
+            cur_skew = WALL_DIST - so1_dist
+            left_vel, right_vel = pd_steer(left_vel, right_vel, cur_skew, so1_skew, 1.0, 0.5)
+            so1_skew = cur_skew
+
+            
+        elif so0_dist < FRONT_VALID_READ:
+            print('so0', so0_dist)
+            cur_skew = WALL_DIST - so0_dist
+            left_vel, right_vel = pd_steer(left_vel, right_vel, cur_skew, so0_skew, 0.01, 5.0)
+            so0_skew = cur_skew
+
+            parallel_skew = 0
+            so3_skew = 0
+            so2_skew = 0
+        else:
+            # No front read at all (in range). Probably turn left
+            left_vel = MAX_SPEED * 0.5675
+            right_vel = MAX_SPEED
+
+    print('LEFT, RIGHT: ', left_vel, right_vel)
 
     leftWheel.setVelocity(left_vel)
     rightWheel.setVelocity(right_vel)
-
-
-    # Too close to the wall, we need to turn right.
-#    if getDistance(so0) < 0.4:
-#        leftWheel.setVelocity(left_vel + steer)
-#        rightWheel.setVelocity(right_vel - steer)
-#
-#        leftWheel.setVelocity(MAX_SPEED)
-#        rightWheel.setVelocity(MAX_SPEED * 0.9)
-#
-#    # Too far from the wall, we need to turn left.
-#    elif getDistance(so0) > 0.6:
-#        leftWheel.setVelocity(left_vel - steer)
-#        rightWheel.setVelocity(right_vel + steer)
-#
-#        leftWheel.setVelocity(MAX_SPEED * 0.9)
-#        rightWheel.setVelocity(MAX_SPEED)
-#
-#    # We are in the right direction.
-#    else:
-#        left_vel = MAX_SPEED
-#        right_vel = MAX_SPEED
-#
-#        leftWheel.setVelocity(MAX_SPEED)
-#        rightWheel.setVelocity(MAX_SPEED)
 
 # Stop the robot when we are done.
 leftWheel.setVelocity(0)
